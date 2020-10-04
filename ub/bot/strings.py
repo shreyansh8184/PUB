7d6193b5a4b9ca9ded6a9baf80c7615c2c6165b3
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 11:48:23 +0530

    Create strings.py

---
+++ ub/bot/strings.py
@@ -0,0 +1,48 @@
+
+PAYMENT_DETAILS = """
+Payment Details✅
+
+Name : ARAVIND BASAPPA KUNTOJI
+Account No. : 309010816413
+IFSC Code: RATN0000129
+Branch: INCHAGERI
+
+Bank Name : RBL Bank.
+UPI ID : royal69@rbl
+"""
+
+HOW = """
+🔥 How You Guys Can Order Any Product From @JacobCarderReborn
+
+1) You Have To Pay Only RS 500 As Advance.✅
+
+2) Then You Have To Send Your Address,Name, Number & Pincode😘
+
+3) Then I Will Complete Your Order In 10 Minutes 👍
+
+4) Then I'll Send You Screenshot Of Order is Placed Successfully And I'll Also Send My Adhar Card And Pan Card Details For Your Safety ✅
+
+5) Then You Have To Pay The Remaining Amount Means After Booking 😍
+
+🔥 Advance System Is Going On 🔥
+
+Come Fast And Grab The Deal's And Offers
+"""
+
+COD = """
+📌 Q3. Why I Couldn't Do Cash On Delivery?
+
+Answer: I'm Placing Order On Amazon And Paying Full Money First Via Hacked Credit Card, So How I Can I Do COD Atleast Think With Some Logic And Sense.
+"""
+
+FORMAT = """
+🔵 If You Are Ready To Pay Money Now Send Your Details In Following Format:
+
+▪️Name:
+▪️Mobile Number:
+▪️Pin Code:
+▪️Flat/House No./Building:
+▪️Colony, Street, Locality:
+▪️Landmark:
+▪️City:
+"""
