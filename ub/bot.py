1ba1487a7be7ce1b5e119db11428c084e3fa1f09
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 11:43:10 +0530

    Update bot.py

--- ub/bot.py
+++ ub/bot.py
@@ -1,1 +1,21 @@
+from ub import app
+from pyrogram import filters
+
+@app.on_message(filters.me & filters.command(["payment"], "."))
+async def payment(client, message):
+    await message.edit(PAYMENT_DETAILS)
+
+@app.on_message(filters.me & filters.command(["how"], "."))
+async def how(client, message):
+    await message.edit(HOW)
+
+@app.on_message(filters.me & filters.command(["cod"], "."))
+async def cod(client, message):
+    await message.edit(COD)
+
+@app.on_message(filters.me & filters.command(["cod"], "."))
+async def format(client, message):
+    await message.edit(FORMAT)
+
+
 
