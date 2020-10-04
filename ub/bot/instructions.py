8de1c0c3f16fa943ab9f8d764a5d56543045d8ae
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 11:49:24 +0530

    Update instructions.py

--- ub/bot/instructions.py
+++ ub/bot/instructions.py
@@ -1,6 +1,8 @@
 from ub import app
 from pyrogram import filters
 
+from ub.bot.strings import (PAYMENT_DETAILS, HOW, COD, FORMAT)
+
 @app.on_message(filters.me & filters.command(["payment"], "."))
 async def payment(client, message):
     await message.edit(PAYMENT_DETAILS)
