259cf44ef884c08f206bc4f4f7fd67c63af782a5
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 23:01:03 +0530

    Update __main__.py

--- ub/__main__.py
+++ ub/__main__.py
@@ -2,7 +2,7 @@ import sys
 from ub import app, LOGGER
 from pyrogram import idle
 
-from ub.bot import instructions
+from ub.bot import instructions, eval
 
 if len(sys.argv) not in (1, 3, 4):
     quit(1)
